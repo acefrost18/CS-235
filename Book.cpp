f0df46a343aba1fae336db9c1001ef97dda47bb4
acefrost18 <65608310+acefrost18@users.noreply.github.com>
2023-02-09 15:35:45 -0500

    Add files via upload

--- Book.cpp
+++ Book.cpp
@@ -1,49 +1,89 @@
 #include "Book.hpp"
 
+/*
+            Default Constructor
+*/ 
 Book::Book() {
 }
 
-Book::Book(const string bookTitle,const string author,const int pageCount,bool digitalFlag) {
-    
-    setTitle(bookTitle);
+/*
+    Parameterized Constructor: Takes 4 parameters
+    @param: The title of the book as a string
+    @param: the author of the book as a string
+    @param: The number of pages in the book(positive int)
+    @param: A flag that indicates if the book is in digital form(Boolean). 
+    Default set to false
+     @post: The private members of the object are set to the values of the parameters.       
+*/ 
+Book::Book(const string book_title,const string author,const int PAGE_COUNT,bool digital_flag) {
+    setTitle(book_title);
     setAuthor(author);
-    setPageCount(pageCount);
-    this->digitalFlag = digitalFlag;
+    setPageCount(PAGE_COUNT);
+    this->digital_flag = digital_flag;
 
 }
 
-void Book::setTitle(const string& bookTitle) {
-    this->bookTitle = bookTitle;
+/*
+    @post:  Sets the title of the book to the parameter
+    @param: Title of the book
+*/ 
+void Book::setTitle(const string& book_title) {
+    this->book_title = book_title;
+    
 }
 
-void Book::setPageCount(const int& pageCount) {
-    if(pageCount > 0) {
-        this->pageCount = pageCount;
+/*
+    @pre: Page count > 0
+    @post:  Sets the page count of the book
+    @param: Number of pages(Positive integer)
+*/ 
+void Book::setPageCount(const int& PAGE_COUNT) {
+    if(PAGE_COUNT > 0) {
+        this->PAGE_COUNT = PAGE_COUNT;
     }
     
 }
 
+/*
+    @post:  Sets the author of the book to the parameter
+    @param: Name of author
+*/ 
 void Book::setAuthor(const string& author) {
     this->author = author;
 }
 
+/*
+    @post: Sets digital flag to true
+*/
 void Book::setDigital() {
-    this->digitalFlag = true;
+    this->digital_flag = true;
 }
 
+/*
+    @returns: Title of the book
+*/ 
 string Book::getTitle() const {
-    return bookTitle;
+    return book_title;
 }
 
+/*
+    @returns: Author of the book
+*/ 
 string Book::getAuthor() const {
     return author;
 }
 
+/*
+    @returns: Number of pages
+*/ 
 int Book::getPageCount() const {
-    return pageCount;
+    return PAGE_COUNT;
 }
 
+/*
+    @returns: Returns the value of digital flag
+*/ 
 bool Book:: isDigital() const {
-    return digitalFlag;
+    return digital_flag;
 }
 
--- Book.hpp
+++ Book.hpp
@@ -10,20 +10,70 @@ using namespace std;
 
 class Book {
     private:
-        string bookTitle;
+        string book_title;
         string author;
-        int pageCount;
-        bool digitalFlag;
+        int PAGE_COUNT;
+        bool digital_flag;
     public:
+        /*
+            Default Constructor
+        */ 
         Book();
-        Book(const string bookTitle,const string author,const int pageCount,bool digitalFlag = false);
-        void setTitle(const string &bookTitle);
+
+
+        /*
+            Parameterized Constructor: Takes 4 parameters
+            @param: The title of the book as a string
+            @param: the author of the book as a string
+            @param: The number of pages in the book(positive int)
+            @param: A flag that indicates if the book is in digital form(Boolean). 
+            Default set to false
+            @post: The private members of the object are set to the values of the parameters.       
+        */ 
+        Book(const string book_title,const string author,const int PAGE_COUNT,bool digital_flag = false);
+        
+        /*
+            @post:  Sets the title of the book to the parameter
+            @param: Title of the book
+        */ 
+        void setTitle(const string &book_title);
+        
+        /*
+            @returns: Title of the book
+        */ 
         string getTitle() const;
+
+        /*
+            @post:  Sets the author of the book to the parameter
+            @param: Name of author
+        */ 
         void setAuthor(const string &author);
+
+        /*
+            @returns: Author of the book
+        */ 
         string getAuthor() const;
-        void setPageCount(const int &pageCount) ;
+
+        /*
+            @pre: Page count > 0
+            @post:  Sets the page count of the book
+            @param: Number of pages(Positive integer)
+        */ 
+        void setPageCount(const int &PAGE_COUNT) ;
+        
+        /*
+            @returns: Number of pages
+        */ 
         int getPageCount() const;
+        
+        /*
+            @post: Sets digital flag to true
+        */
         void setDigital();
+        
+        /*
+            @returns: Returns the value of digital flag
+        */ 
         bool isDigital() const;
 };
 #endif
